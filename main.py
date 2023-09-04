ff7981905675549b5cf9f3dd0b0e83c03f38e871
InvincibleCookie <stav2004@mail.ru>
2023-09-05 00:18:06 +0300

    add show_random_photo to main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from ryan_gosling_movies import get_movies
-
+from ryan_gosling_photos import show_random_photo
 
 def main():
     choice = input()
     if choice == "1":
         get_movies()
+    elif choice == "2":
+        show_random_photo()
     else:
         print("Неправильный выбор")
 

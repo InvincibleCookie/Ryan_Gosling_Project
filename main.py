b00144a50f39f93c73d1adac1ed6d48091f529cd
InvincibleCookie <stav2004@mail.ru>
2023-09-05 08:42:20 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from ryan_gosling_movies import get_movies
 from ryan_gosling_photos import show_random_photo, show_specific_photo
 
 def main():
-    choice = input()
+    print("Добро пожаловать в мир Райана Гослинга!")
+    print("Выберите, что вы хотели бы увидеть:")
+    print("1. Фильмы")
+    print("2. Фотографии")
+    print("3. Открыть конкретную фотографию")
+
+    choice = input("Введите номер вашего выбора: ")
+
     if choice == "1":
         get_movies()
     elif choice == "2":
@@ -11,8 +18,7 @@ def main():
         photo_name = input("Введите имя файла фотографии: ")
         show_specific_photo(photo_name)
     else:
-        print("Неправильный выбор")
-
+        print("Неправильный выбор. Пожалуйста, введите 1, 2 или 3.")
 
 if __name__ == "__main__":
     main()
